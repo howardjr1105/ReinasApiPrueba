b47d49dd20ad2c076746b9129fd21ec122a6fff0
Abel Marin <djexworld@hotmail.com>
2025-08-19 17:55:05 -0600

    Agregué .gitignore y la cadena de conexión

--- ReinasApiPrueba/obj/Debug/net6.0/ReinasApiPrueba.AssemblyInfo.cs
+++ ReinasApiPrueba/obj/Debug/net6.0/ReinasApiPrueba.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReinasApiPrueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e0fefdc268a544f7e451d817dad27bf928dcf1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReinasApiPrueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReinasApiPrueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
